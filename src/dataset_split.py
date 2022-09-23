293b73a66e9a08482c1bc9a2799aa845e19ff164
Yuhyeong <lemar14r@gmail.com>
2022-09-23 21:30:45 +0800

    为 utils/dataset_split.py增加注释

--- src/dataset_split.py
+++ src/dataset_split.py
@@ -1,18 +1,22 @@
 import os
 import shutil
 
-
+# 分离一部分训练集为验证集
 def split2val(train_dir, val_dir):
-    img_list = os.listdir(os.path.join(train_dir, 'data'))
-    label_list = os.listdir(os.path.join(train_dir, 'label'))
 
+    # 训练集图片、标签目录
     train_img_dir = os.path.join(train_dir, 'data')
     train_label_dir = os.path.join(train_dir, 'label')
 
+    # 训练集图片、标签列表
+    img_list = os.listdir(train_img_dir)
+    label_list = os.listdir(train_label_dir)
 
+    # 验证集图片、标签目录
     val_img_dir = os.path.join(val_dir, 'data')
     val_label_dir = os.path.join(val_dir, 'label')
 
+    # 遍历训练集，每50个转移1个到训练集
     i = 0
     for img_name in img_list:
         label_name = img_name[:-3] + 'txt'
